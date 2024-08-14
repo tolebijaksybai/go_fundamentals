379eef59329a602d6dbf99013a4aeef116b90d47
Tolebi <tolebizaksybaj@gmail.com>
2024-08-14 18:45:48 +0500

    add lesson 1

--- 1/main.go
+++ 1/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unsafe"
+)
 
+var name = "Global variable"
+
+const age = 4
+
+func main() {
+	var num1 int64 = 15
+	var num2 int = 13
+
+	getType(num1 + int64(num2))
+
+	fmt.Println(unsafe.Sizeof(uint8(num1)))
+
+	surname := "Vasia"
+	hello := fmt.Sprintf("hello world %s", surname)
+	_ = hello // игнорирует
+
+	fmt.Println(hello)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	name := "No global variable"
+	fmt.Println(name)
+
+	var nameString = "Tolebi"
+	getType(nameString)
+
+	nameString2 := "Tolebi"
+	getType(nameString2)
+
+	var isBool = true
+	getType(isBool)
+
+	isBool2 := true
+	getType(isBool2)
+}
+
+func getType(param any) {
+	fmt.Printf("Type: %T | Value: %v\n", param, param)
 }
